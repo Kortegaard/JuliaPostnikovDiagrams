535719cd4ac04a06f232fe53a1ac8674adf9fff6
Kortegaard <ank@askapps.eu>
2022-04-18 13:15:36 +0200

    mac changes

--- main.jl
+++ main.jl
@@ -1,12 +1,12 @@
 include("postnikov.jl");
 include("postnikovQuiver.jl");
-include("maxcollection.jl")
-include("out412.jl")
-include("out312.jl")
-include("out510.jl")
+include("maxcollections/maxcollection.jl")
+include("maxcollections/out412.jl")
+include("maxcollections/out312.jl")
+include("maxcollections/out510.jl")
 
 function drawFourDiags(k,n,coll, fileName)
-    fig = Figure(resolution = (1600, 1600), backgroundcolor = :transparent,figure_padding = 1);
+    fig = Figure(resolution = (1600, 800), backgroundcolor = :transparent,figure_padding = 1);
 
     ax = Axis(fig[1, 1], backgroundcolor = :transparent)
     hidedecorations!(ax)
@@ -37,34 +37,34 @@ function drawFourDiags(k,n,coll, fileName)
     );
     
 
-    ax3 = Axis(fig[2, 1], backgroundcolor = :transparent)
-    hidedecorations!(ax3)
-    hidespines!(ax3)
-    ax3.xticksvisible = false
-    ax3.yticksvisible = false
-    ax3.backgroundcolor = :transparent
-    poshDrag = drawPostnikovDiagram(k,n,coll,
-        showPostnikovQuiver         = true,
-        showPlabicGraph             = false, 
-        showPostnikovDiagram        = true, 
-        showPostnikovDiamgramArrows = true,
-        fig = fig
-    );
+    # ax3 = Axis(fig[2, 1], backgroundcolor = :transparent)
+    # hidedecorations!(ax3)
+    # hidespines!(ax3)
+    # ax3.xticksvisible = false
+    # ax3.yticksvisible = false
+    # ax3.backgroundcolor = :transparent
+    # poshDrag = drawPostnikovDiagram(k,n,coll,
+    #     showPostnikovQuiver         = true,
+    #     showPlabicGraph             = false, 
+    #     showPostnikovDiagram        = true, 
+    #     showPostnikovDiamgramArrows = true,
+    #     fig = fig
+    # );
 
 
-    ax4 = Axis(fig[2, 2], backgroundcolor = :transparent)
-    hidedecorations!(ax4)
-    hidespines!(ax4)
-    ax4.xticksvisible = false
-    ax4.yticksvisible = false
-    ax4.backgroundcolor = :transparent
-    poshDrag = drawPostnikovDiagram(k,n,coll,
-        showPostnikovQuiver         = true,
-        showPlabicGraph             = true, 
-        showPostnikovDiagram        = false, 
-        showPostnikovDiamgramArrows = true,
-        fig = fig
-    );
+    # ax4 = Axis(fig[2, 2], backgroundcolor = :transparent)
+    # hidedecorations!(ax4)
+    # hidespines!(ax4)
+    # ax4.xticksvisible = false
+    # ax4.yticksvisible = false
+    # ax4.backgroundcolor = :transparent
+    # poshDrag = drawPostnikovDiagram(k,n,coll,
+    #     showPostnikovQuiver         = true,
+    #     showPlabicGraph             = true, 
+    #     showPostnikovDiagram        = false, 
+    #     showPostnikovDiamgramArrows = true,
+    #     fig = fig
+    # );
 
 
     save(fileName, fig);
@@ -72,13 +72,13 @@ end
 
 
 k = 3
-n = 12
-cl = upToEquiv(k,n,col312)
+n = 9
+cl = upToEquiv(k,n,col39)
 
 println("There are " * string(length(cl)) * " diagram")
 for i in 1:length(cl)
     println("Drawing number: " * string(i) * " / " * string(length(cl)))
-    drawFourDiags(k,n,cl[i], "312_postnikov/" * string(i) * ".pdf")
+    drawFourDiags(k,n,cl[i], "39_postnikov/" * string(i) * ".pdf")
 end
 
 
